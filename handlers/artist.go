93ef80a9fad47755507ce27f0631ac2777f398f5
donaldiorwua <donaldiorwua@gmail.com>
2026-09-04 14:59:57 +0100

    added artist API get request logic

---
+++ handlers/artist.go
@@ -0,0 +1,45 @@
+package handlers
+
+import(
+	"io"
+	"fmt"
+	"net/http"
+	"encoding/json"
+)
+
+
+	type Artist struct {
+		ID           int      `json:"id"`
+		Name        string   `json:"name"`
+		CreationYear int      `json:"creation_year"`
+		Members     []string `json:"members"`
+	}
+
+
+func FetchArtists() ([]Artist, error) {
+	response, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+	if err != nil {
+		fmt.Println("Error fetching data:", err)
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("non-OK HTTP status: %s", response.Status)
+	}
+	
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var artists []Artist
+	
+	if err := json.Unmarshal(body, &artists); err != nil {
+		return nil, err
+	}
+
+
+	return artists, nil
+}
