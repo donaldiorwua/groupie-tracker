ad46924d66455fde1389e2c792debbc7579c3401
donaldiorwua <donaldiorwua@gmail.com>
2026-09-04 15:01:18 +0100

    updated main logic

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
-import "fmt"
 
+import(
+	"fmt"
+	"groupie-tracker/handlers"
+)
 
-func main()  {
-	fmt.Println("Hello World")
+func main() {
+	artists, err := handlers.FetchArtists()
+	if err != nil {
+		fmt.Println("Error fetching artists:", err)
+		return
+	}
+
+	for _, artist := range artists {
+		fmt.Printf("ID: %d, Name: %s, Creation Year: %d, Members: %v\n", artist.ID, artist.Name, artist.CreationYear, artist.Members)
+	}
 }
