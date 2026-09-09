5c2754ff9b93314bc02a771ebf7a6c888dbbcc05
donaldiorwua <donaldiorwua@gmail.com>
2026-09-09 18:45:32 +0100

    Updated main file!

--- main.go
+++ main.go
@@ -7,14 +7,12 @@ import (
 )
 
 func main() {
-	//var err error
-
-	//webhandlers.Temp = template.Must(template.ParseGlob("templates/*html"))
 
 	fmt.Println("server running at http://localhost:8080/")
 
 	//http.HandleFunc("/", ArtistProfile)
 	http.HandleFunc("/artists", webhandlers.Artists)
+	http.HandleFunc("/artist", webhandlers.ArtistProfile)
 
 	http.ListenAndServe(":8080", nil)
 }
