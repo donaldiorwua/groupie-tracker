ebe19d58da4463f734e9cada3df414e1e53d6f2f
donaldiorwua <donaldiorwua@gmail.com>
2026-09-10 18:38:42 +0100

    updated error handling

--- webhandlers/artistProfileHandler.go
+++ webhandlers/artistProfileHandler.go
@@ -1,9 +1,9 @@
 package webhandlers
 
 import (
+	"errors"
 	"fmt"
-	"groupie-tracker/apihandlers"
-	"groupie-tracker/models"
+	"groupie-tracker/lookup"
 	"html/template"
 	"log"
 	"net/http"
@@ -17,63 +17,22 @@ func ArtistProfile(w http.ResponseWriter, r *http.Request) {
 		strID := r.URL.Query().Get("id")
 
 		intID, err := strconv.Atoi(strID)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			fmt.Fprint(w, err.Error())
+		if err != nil || intID <= 0 {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprint(w, "Invalid artist ID")
 			return
 		}
 
-		artists, err := apihandlers.FetchArtists()
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			fmt.Fprint(w, err.Error())
-			return
-		}
-
-		found := false
-		var selectedArtist models.Artist
-		for _, artist := range artists {
-			if artist.ID == intID {
-				selectedArtist = artist
-				found = true
-				break
-			}
-		}
-		if !found {
+		artistProfile, err := lookup.FindArtistProfile(intID)
+		if errors.Is(err, lookup.ErrArtistNotFound) {
 			w.WriteHeader(http.StatusNotFound)
 			fmt.Fprint(w, "Artist not found")
 			return
 		}
-
-		relations, err := apihandlers.FetchRelations()
+		
 		if err != nil {
-			fmt.Println("Error fetching relations:", err)
-			return
-		}
-
-		relationsByID := make(map[int]models.Relation, len(relations.Index))
-		for _, relation := range relations.Index {
-			relationsByID[relation.ID] = relation
-		}
-
-		var artistProfile models.ConcertInfo
-		relation, ok := relationsByID[selectedArtist.ID]
-		if ok {
-			artistInfo := models.ConcertInfo{
-				ID:             selectedArtist.ID,
-				Image:			selectedArtist.Image,
-				Name:           selectedArtist.Name,
-				CreationYear:   selectedArtist.CreationYear,
-				FirstAlbum: 	selectedArtist.FirstAlbum,
-				Members:        selectedArtist.Members,
-				DatesLocations: relation.DatesLocations,
-			}
-
-		artistProfile = artistInfo
-
-		} else {
-			w.WriteHeader(http.StatusNotFound)
-			fmt.Fprint(w, "No Artist information found")
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w, "Internal server error!")
 			return
 		}
 
