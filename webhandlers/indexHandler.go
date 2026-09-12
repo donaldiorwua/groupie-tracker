0b3db994e3df5eeae304f74aabaa163fc638db90
donaldiorwua <donaldiorwua@gmail.com>
2026-09-12 23:20:11 +0100

    updated index page hnadler

--- webhandlers/indexHandler.go
+++ webhandlers/indexHandler.go
@@ -1,9 +1,30 @@
 package webhandlers
 
 import (
+	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 )
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
 
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprint(w, "Method not allowed")
+		return
+	}
+
+	tmpl, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, err.Error())
+		return
+	}
+
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
 }
