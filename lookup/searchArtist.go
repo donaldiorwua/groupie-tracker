95130a3e937d1d9abd85ff446e50933a04f844c5
donaldiorwua <donaldiorwua@gmail.com>
2026-09-14 12:13:21 +0100

    Adjusted search artist function and added more search modes

--- lookup/searchArtist.go
+++ lookup/searchArtist.go
@@ -2,16 +2,35 @@ package lookup
 
 import (
 	"groupie-tracker/models"
+	"strconv"
 	"strings"
 )
 
 func SearchArtists(artists []models.Artist, search string) []models.Artist {
 	filteredArtists := []models.Artist{}
-	for _, artist := range artists {
-		if strings.Contains(
-			strings.ToLower(artist.Name),
-			strings.ToLower(search)) {
-			filteredArtists = append(filteredArtists, artist)
+	searchYear, err := strconv.Atoi(search)
+	if err == nil {
+		for _, artist := range artists {
+			if searchYear == artist.CreationYear {
+				filteredArtists = append(filteredArtists, artist)
+				break
+			}
+		}
+	} else {
+		for _, artist := range artists {
+			if strings.Contains(
+				strings.ToLower(artist.Name),
+				strings.ToLower(search)) {
+				filteredArtists = append(filteredArtists, artist)
+			}
+			for _, member := range artist.Members {
+				if strings.Contains(
+					strings.ToLower(member),
+					strings.ToLower(search)) {
+					filteredArtists = append(filteredArtists, artist)
+					break
+				}
+			}
 		}
 	}
 	return filteredArtists
